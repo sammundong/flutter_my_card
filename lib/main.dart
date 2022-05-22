48357eae88553ac5835d7f1926efa1788b0a2b2f
iseungmin <lsls011031@gmail.com>
2022-05-22 19:32:36 +0900

    card를 이용함

--- lib/main.dart
+++ lib/main.dart
@@ -9,29 +9,78 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.black,
           body: SafeArea(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center ,
             children: <Widget>[
               CircleAvatar(
-                radius: 50.0,
+                radius: 80.0,
                 backgroundImage: AssetImage('images/ralo.jpg'),
               ),
               Text(
-                'ralo',
+                'GreenDay-2400',
                 style: TextStyle(
                   fontSize: 40.0,
                   fontFamily: 'Pacifico',
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
-              )
-
+              ),
+              Text(
+                'Season 2',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100 ,
+              ),),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      '+ 010 4830 672*',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  )),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      'lsls011031@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  )),
             ],
-          )
-       )
-      )
-      ,
+          ))),
     );
   }
 }
